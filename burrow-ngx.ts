1d0f800c443f5b0b2bbcbdb5d0e1fb8dafd286d7
Charlotte Som <charlotte@som.codes>
2025-07-18 20:34:19 -0700

    add index route and read version from deno.json

---
+++ burrow-ngx.ts
@@ -0,0 +1,24 @@
+import ngx from "jsr:@char/ngx@0.2";
+
+const domain = "x-burrow-20250718.bun.how";
+export const config = ngx("", [
+  ngx("map $http_upgrade $connection_upgrade", ["default upgrade", "'' close"]),
+  ngx("server", [
+    ...ngx.listen(),
+    ...ngx.letsEncrypt(domain),
+    ngx.serverName(domain),
+    ngx("location /", [
+      "client_max_body_size 1G",
+      "proxy_pass http://127.0.0.1:3000",
+      "proxy_http_version 1.1",
+      "proxy_set_header Upgrade $http_upgrade",
+      "proxy_set_header Connection $connection_upgrade",
+      "proxy_set_header Host $host",
+      "proxy_read_timeout 300s",
+    ]),
+  ]),
+]);
+
+if (import.meta.main) {
+  console.log(config.build());
+}
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,25 @@ import { appConfig } from "./config.ts";
 import { Application, Router } from "@oak/oak";
 import { XRPCRouter } from "./xrpc-server.ts";
 
+import denoJson from "../deno.json" with { type: "json" };
+const VERSION = denoJson.version;
+
 const app = new Application();
 const xrpc = new XRPCRouter();
 const router = new Router();
 
-xrpc.query({ method: "_health" }, () => ({ version: "burrow 0.1.0" }));
+xrpc.query({ method: "_health" }, () => ({ version: "burrow " + VERSION }));
+
+router.get("/", ctx => {
+  ctx.response.type = "text/plain; charset=utf-8";
+  ctx.response.body = `burrow pds version ${VERSION}
+
+  /)/)
+( . .)
+( づ♡
+ 
+this is an atproto PDS ^-^ see atproto.com`;
+});
 
 app.use(xrpc.middleware());
 app.use(router.routes());
